d9457440af7a89dde14d8231a41950db43a0dc00
Marcin Pudełek <mrcnpdlk@gmail.com>
2017-10-27 21:43:40 +0200

    directory structure

---
+++ src/MojePanstwo/Client.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * MOJEPANSTWO-API
+ *
+ * Copyright (c) 2017 pudelek.org.pl
+ *
+ * @license MIT License (MIT)
+ *
+ * For the full copyright and license information, please view source file
+ * that is bundled with this package in the file LICENSE
+ *
+ * @author  Marcin Pudełek <marcin@pudelek.org.pl>
+ */
+declare (strict_types=1);
+
+namespace mrcnpdlk\MojePanstwo;
+
+
+class Client
+{
+
+}
---
+++ src/MojePanstwo/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * MOJEPANSTWO-API
+ *
+ * Copyright (c) 2017 pudelek.org.pl
+ *
+ * @license MIT License (MIT)
+ *
+ * For the full copyright and license information, please view source file
+ * that is bundled with this package in the file LICENSE
+ *
+ * @author Marcin Pudełek <marcin@pudelek.org.pl>
+ */
+
+namespace mrcnpdlk\MojePanstwo;
+
+
+class Exception extends \Exception
+{
+
+}
